7bc5c560439644e3a08f6ba83edf91c09ba143e6
ansh2701 <agrawalansh2701@gmail.com>
2021-06-29 16:09:19 +0530

    first commit

---
+++ components/Neon.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Neon.module.css";
+const Neon = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.neon}>Cup</div>
+      <div className={styles.flux}>TicTacToe</div>
+    </div>
+  );
+};
+
+export default Neon;
